a37d5435a2c98b6b56b63294b1f81557c8e9da95
m-e-h <info@martyhelmick.com>
2018-06-27 11:05:38 -0400

    inline docs and rename smartthings mqtt bridge

--- smartthings-bridgio/run.sh
+++ smartthings-bridgio/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+# Smartthings MQTT Bridge accepts a config.yml for user configs.
+# Lets create one from our options.json.
 CONFIG_PATH=/data/options.json
 
+# Get the user configs from JSON.
 export MQTT_HOST=`node -p "require('$CONFIG_PATH').broker_host"`
 export PREFACE=`node -p "require('$CONFIG_PATH').preface"`
 export BRIDGE_PORT=`node -p "require('$CONFIG_PATH').bridge_port"`
@@ -12,6 +15,7 @@ export COMMAND=`node -p "require('$CONFIG_PATH').command_suffix"`
 export USERNAME=`node -p "require('$CONFIG_PATH').username"`
 export PASSWORD=`node -p "require('$CONFIG_PATH').password"`
 
+# Push the JSON data to the yaml template and create a config.yml.
 rm -f /data/config.yml temp.yml
 ( echo "cat <<EOF >/data/config.yml";
   cat /temp/template.yml;
@@ -20,4 +24,5 @@ rm -f /data/config.yml temp.yml
 . temp.yml
 cat /data/config.yml
 
+# Run the node app.
 smartthings-mqtt-bridge
