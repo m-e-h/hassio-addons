de07e339faea32762d596e569b67e9f29954f192
m-e-h <info@martyhelmick.com>
2018-09-05 17:47:54 -0400

    Make Dockerfile more hassio friendly

--- smartthings-bridgio/run.sh
+++ smartthings-bridgio/run.sh
@@ -3,17 +3,17 @@ set -e
 
 # Smartthings MQTT Bridge accepts a config.yml for user configs.
 # Lets create one from our options.json.
-CONFIG_PATH=/data/options.json
+CONFIG_OPTIONS=/data/options.json
 
 # Get the user configs from JSON.
-export MQTT_HOST=`node -p "require('$CONFIG_PATH').broker_host"`
-export PREFACE=`node -p "require('$CONFIG_PATH').preface"`
-export BRIDGE_PORT=`node -p "require('$CONFIG_PATH').bridge_port"`
-export READ_STATE=`node -p "require('$CONFIG_PATH').state_read_suffix"`
-export WRITE_STATE=`node -p "require('$CONFIG_PATH').state_write_suffix"`
-export COMMAND=`node -p "require('$CONFIG_PATH').command_suffix"`
-export USERNAME=`node -p "require('$CONFIG_PATH').username"`
-export PASSWORD=`node -p "require('$CONFIG_PATH').password"`
+export MQTT_HOST=`node -p "require('$CONFIG_OPTIONS').broker_host"`
+export PREFACE=`node -p "require('$CONFIG_OPTIONS').preface"`
+export BRIDGE_PORT=`node -p "require('$CONFIG_OPTIONS').bridge_port"`
+export READ_STATE=`node -p "require('$CONFIG_OPTIONS').state_read_suffix"`
+export WRITE_STATE=`node -p "require('$CONFIG_OPTIONS').state_write_suffix"`
+export COMMAND=`node -p "require('$CONFIG_OPTIONS').command_suffix"`
+export USERNAME=`node -p "require('$CONFIG_OPTIONS').username"`
+export PASSWORD=`node -p "require('$CONFIG_OPTIONS').password"`
 
 # Push the JSON data to the yaml template and create a config.yml.
 rm -f /data/config.yml temp.yml
