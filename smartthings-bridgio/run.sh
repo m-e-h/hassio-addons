fdf38b40eee3be08a3896a0eabdb53a71a02cef3
m-e-h <info@martyhelmick.com>
2018-09-06 17:13:58 -0400

    correct some variable names and add the retain flag

--- smartthings-bridgio/run.sh
+++ smartthings-bridgio/run.sh
@@ -6,14 +6,16 @@ set -e
 CONFIG_OPTIONS=/data/options.json
 
 # Get the user configs from JSON.
-export MQTT_HOST=`node -p "require('$CONFIG_OPTIONS').broker_host"`
+export BROKER_HOST=`node -p "require('$CONFIG_OPTIONS').broker_host"`
+export BROKER_PORT=`node -p "require('$CONFIG_OPTIONS').broker_port"`
 export PREFACE=`node -p "require('$CONFIG_OPTIONS').preface"`
-export BRIDGE_PORT=`node -p "require('$CONFIG_OPTIONS').bridge_port"`
 export READ_STATE=`node -p "require('$CONFIG_OPTIONS').state_read_suffix"`
 export WRITE_STATE=`node -p "require('$CONFIG_OPTIONS').state_write_suffix"`
 export COMMAND=`node -p "require('$CONFIG_OPTIONS').command_suffix"`
 export USERNAME=`node -p "require('$CONFIG_OPTIONS').username"`
 export PASSWORD=`node -p "require('$CONFIG_OPTIONS').password"`
+export RETAIN=`node -p "require('$CONFIG_OPTIONS').retain"`
+export BRIDGE_PORT=`node -p "require('$CONFIG_OPTIONS').bridge_port"`
 
 # Push the JSON data to the yaml template and create a config.yml.
 rm -f /data/config.yml temp.yml
